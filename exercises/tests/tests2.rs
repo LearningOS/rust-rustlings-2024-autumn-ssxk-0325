1ffc6d18ed5f4bd769834bdba1d29ee31363423e
202212900705-chenyongming <2512739663@qq.com>
2024-10-14 19:37:11 +0800

    update

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,14 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
+    use std::fmt::format;
+
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(format!("s"),"s".to_string());
     }
 }
